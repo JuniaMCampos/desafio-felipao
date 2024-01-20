b9285da6db39602266a4ff0676bef8d6fb7922e8
JuniaMCampos <juniacamposjc@gmail.com>
2024-01-20 17:47:59 -0300

    desafio do felipao devidamente estruturado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
-let nome = "felipe"
-console.log(nome)
+let nomeHeroi = "Felipe";
+let xpDoHeroi = 8000;
+
+let nivelDoHeroi;
+
+switch (true) {
+    case (xpDoHeroi < 1000):
+      nivelDoHeroi = "Ferro";
+      break;
+    case (xpDoHeroi >= 1001 && xpDoHeroi <= 2000):
+      nivelDoHeroi = "Bronze";
+      break;
+    case (xpDoHeroi >= 2001 && xpDoHeroi <= 5000):
+        nivelDoHeroi= "Prata";
+        break;
+    case (xpDoHeroi >= 5001 && xpDoHeroi <= 7000):
+        nivelDoHeroi = "Ouro";
+        break;
+    case (xpDoHeroi >= 7001 && xpDoHeroi <= 8000):
+        nivelDoHeroi = "Platina";
+        break;
+    case (xpDoHeroi >= 8001 && xpDoHeroi <= 9000):
+        nivelDoHeroi = "Ascendente";
+        break;
+    case (xpDoHeroi >= 9001 && xpDoHeroi <= 10000):
+        nivelDoHeroi = "Imortal";
+        break; 
+    case (xpDoHeroi >= 10001):
+        nivelDoHeroi = "Radiante";
+        break;       
+    default:
+      console.log("Nenhum XP informado");
+  }
+
+  console.log("O Herói de nome " + nomeHeroi + " está no nível de " + nivelDoHeroi)
